9910f160aa7969ffe76d7d832a284a31e3582d3c
hmzdnzl <hmzdnzl38@gmail.com>
2025-10-28 00:34:29 +0300

    tüm  görevler tamamlandı

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,9 +6,9 @@ public class Main {
     }
 
     public static boolean shouldWakeUp(boolean isBarking, int clock) {
-        if (clock <0 && clock >23 ) {
+        if (clock < 0 || clock > 23) {
             return false;
-        } else if (clock >=20 && clock<= 8) {
+        } else if (isBarking && (clock < 8 || clock > 22)) {
             return true;
         } else {
             return false;
@@ -16,18 +16,41 @@ public class Main {
     }
 
     public static boolean hasTeen(int firstAge, int secondAge, int thirdAge) {
-       
+        if ((firstAge >= 13 && firstAge <= 19) ||
+                (secondAge >= 13 && secondAge <= 19) ||
+                (thirdAge >= 13 && thirdAge <= 19)) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
     public static boolean isCatPlaying(boolean isSummer, int temp) {
-     
+        if (isSummer == true) {
+            if (temp >= 25 && temp <= 45) {
+                return true;
+            }
+        } else if (isSummer == false) {
+            if (temp >= 25 && temp <= 35) {
+                return true;
+            }
+        }
+        return false;
     }
 
     public static double area(double width, double height) {
-       
+        if (width<0 || height<0) {
+            return -1;
+        } else {
+            return width * height;
+        }
     }
 
     public static double area(double radius) {
-       
+        if (radius <0) {
+            return -1;
+        } else {
+            return radius*radius*Math.PI;
+        }
     }
 }
